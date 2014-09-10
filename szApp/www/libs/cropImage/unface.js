e71fe92d8f36b2dc23c82e0d8eea52017b9f4ad8
beebikun <beebikun@gmail.com>
2014-09-10 21:09:22 +1000

    добавлено ограничение на предвижение и изменение размера для face

--- szApp/www/libs/cropImage/unface.js
+++ szApp/www/libs/cropImage/unface.js
@@ -18,15 +18,17 @@ var UnfaceImage = Imagable.extend({
         facesArea.className = 'facesArea';
 
         var self = this;
-        this._mousedown(facesArea, function(ev){
-            ev.preventDefault();
-            self._clearState()
-            var x = ev.pageX - self.body.left;
-            var y = ev.pageY - self.body.top;
-            var face = self._createFace(x, y)
+        this._mousedown(this.body, function(ev){
+            if(!self.is_down){
+                ev.preventDefault();
+                self._clearState()
+                var x = ev.pageX - self.body.left;
+                var y = ev.pageY - self.body.top;
+                var face = self._createFace(x, y)
+            }
         })
         this._mouseup(facesArea)
-        this._mousemove(facesArea, function(ev){self._moveFace(ev)})
+        this._mousemove(this.body, function(ev){self._moveFace(ev)})
         this.body.appendChild(facesArea)
     },
     _preDraw: function(){
@@ -35,19 +37,25 @@ var UnfaceImage = Imagable.extend({
         this.body.top = bodyRect.top;
         this.body.left = bodyRect.left;
     },
-    _moveFace: function(ev, s){
+    _moveFace: function(ev){
         if(this.movedFace){
             this._clearTimer()
-            var x = ev.pageX - this.body.left;
-            var y = ev.pageY - this.body.top;
             var face = this.movedFace;
             var w = face.offsetWidth;
-            face.style.left = x - w/2 + 'px';
-            face.style.top = y - w/2 + 'px';
+            var x = ev.pageX - this.body.left - w/2;
+            var y = ev.pageY - this.body.top - w/2;
+            if(!this._canBeChanged(w, w, x, y)) return
+            face.style.left = x + 'px';
+            face.style.top = y + 'px';
         }
     },
+    _canBeChanged: function(w, h, x, y){
+        return w >= this.settings.face.min && h >= this.settings.face.min &&
+               x > 0 && x + w < this.body.offsetLeft + this.body.offsetWidth &&
+               y > 0 && y + h < this.body.offsetTop + this.body.offsetHeight
+    },
     _createFace: function(x, y){
-        if(!this.activeFace)return
+        if(!this.activeFace || !this.facesArea)return
         var face = document.createElement('img');
         face.setAttribute('src', this.activeFace.face);
         face.face = this.activeFace;
@@ -62,15 +70,17 @@ var UnfaceImage = Imagable.extend({
         var self = this;
         this._mousedown(face, function(ev){
             ev.preventDefault();
+            self.is_down = true
             self.movedFace = this;
             var k = self.settings.face.step, face = this;
 
             self.facetimer = setInterval(function(){
-                var neww = face.offsetWidth + k + 'px';
-                face.style.width = neww;
-                face.style.height = neww;
-                face.style.top = face.offsetTop - k/2 + 'px';
-                face.style.left = face.offsetLeft - k/2 + 'px';
+                var neww = face.offsetWidth + k, newx = face.offsetTop - k/2, newy = face.offsetLeft - k/2;
+                if(!self._canBeChanged(neww, neww, newx, newy)) self._clearTimer()
+                face.style.width = neww + 'px';
+                face.style.height = neww + 'px';
+                face.style.top = newx + 'px';
+                face.style.left = newy + 'px';
             }, self.settings.tm);
         });
         this._mouseup(face);
@@ -85,8 +95,8 @@ var UnfaceImage = Imagable.extend({
     },
     _clearState: function(){
         this._clearTimer()
-        this.dragok = false;
         this.movedFace = undefined;
+        this.is_down = undefined;
     },
     _clearFaces: function(){
         for (var i = this.facesList.length - 1; i >= 0; i--) {
@@ -119,6 +129,6 @@ var UnfaceImage = Imagable.extend({
     zip: function(){
         this._movedFacesToCanvas()
         var base64ImageData = this.canvas.toDataURL();
-        this.updateImage(base64ImageData)
+        this.updateImage(base64ImageData, this.usedFaces)
     },
 })
